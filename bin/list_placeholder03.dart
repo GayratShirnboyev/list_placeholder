7ac533cac6b27f98eb3752105ccf5367235e2c77
G'ayrat <shirnboyevgayrat@gmail.com>
2022-07-16 09:09:12 +0500

    weref

---
+++ bin/list_placeholder03.dart
@@ -0,0 +1,20 @@
+/*
+Create function func 
+Make a list of n ones.
+Argument int n
+Return the List
+
+*/
+List func(int n) {
+  int f = 0;
+  List c = [];
+  while (f <= n) {
+    f++;
+    c.add(1);
+  }
+  return c;
+}
+
+void main() {
+  print(func(5));
+}
