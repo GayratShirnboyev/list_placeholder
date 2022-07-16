3a6ab8fa44e9db2ef07984e2d1e79e1b1d069a68
G'ayrat <shirnboyevgayrat@gmail.com>
2022-07-16 10:41:30 +0500

    serfrf

--- bin/list_placeholder08.dart
+++ bin/list_placeholder08.dart
@@ -9,16 +9,14 @@ List func(n) {
   int f = 0;
   List c = [];
   while (f < n) {
-    f++;
+    c.add(f);
+    f = f + 2;
     print(f);
-    if (f % 2 == 0) {
-      c.add(f - 2);
-    }
   }
 
   return c;
 }
 
 void main() {
-  print(func(6));
+  print(func(7));
 }
