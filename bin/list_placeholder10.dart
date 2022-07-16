4946d5b4e00a88eda9a5225db1e1b9ad816c5425
G'ayrat <shirnboyevgayrat@gmail.com>
2022-07-16 13:14:50 +0500

    df

--- bin/list_placeholder10.dart
+++ bin/list_placeholder10.dart
@@ -3,7 +3,24 @@ Create function func
 Create a list of n elements, even index values are string and odd index values are integers.
 Argument int n
 Return the List
-
 */
 
-void main() {}
+List func(n) {
+  List l = [];
+
+  int x = 0;
+  while (x < n) {
+    if (x % 2 == 0) {
+      l.add(x.toString());
+    } else {
+      l.add(x);
+    }
+    x += 1;
+  }
+  // print(l[2].runtimeType);
+  return l;
+}
+
+void main() {
+  print(func(4));
+}
