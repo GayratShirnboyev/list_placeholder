5ab2ec918d55d3a8a8293af1c1b7e2d847b9fa22
G'ayrat <shirnboyevgayrat@gmail.com>
2022-07-16 13:34:57 +0500

    seergf

--- bin/list_placeholder12.dart
+++ bin/list_placeholder12.dart
@@ -6,4 +6,16 @@ Return the List
 
 */
 
-void main() {}
+List func(int n) {
+  List l = [];
+  int x = 0;
+  while (x < n) {
+    l.add(x * x);
+    x -= 1;
+  }
+  return l;
+}
+
+void main() {
+  print(func(4));
+}
